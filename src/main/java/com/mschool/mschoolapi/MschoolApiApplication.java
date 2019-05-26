e2d1b7aafaeb97a305de5d7bb7f8ae99fef411e0
ndoyeahmed <ndoyeahmed2602@gmail.com>
2019-05-26 18:20:40 +0000

    Initial commit

---
+++ src/main/java/com/mschool/mschoolapi/MschoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.mschool.mschoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MschoolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MschoolApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mschool/mschoolapi/MschoolApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mschool.mschoolapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MschoolApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
